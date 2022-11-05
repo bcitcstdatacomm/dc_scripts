53e48280e356c857a98c10289f1b7d7f4fee00f6
programming101dev <113087924+programming101dev@users.noreply.github.com>
2022-11-05 12:05:08 -0700

    added delete, renamed some flags

--- manage.sh
+++ manage.sh
@@ -30,18 +30,52 @@ cmake_init()
 cmake_build()
 {
   cmake --build "$1"/cmake-build-debug
+  if [ "$?" -ne 0 ]; then
+    printf "ERROR building %s" $2
+    exit 1
+  fi
 }
 
 # $1 directory path
 cmake_install()
 {
   sudo cmake --install "$1"/cmake-build-debug
+
+  if command -v ldconfig &> /dev/null
+  then
+    sudo ldconfig
+  fi
 }
 
 # $1 directory path
 cmake_remove()
 {
   rm -rf "$1"/cmake-build-debug
+}
+
+remove_library()
+{
+  if [ -d "/usr/local/lib/$1" ];
+  then
+    sudo rm -rf /usr/local/lib/$1
+  else
+    sudo rm -f /usr/local/lib/$1
+  fi
+}
+
+remove_includes()
+{
+  if [ -d "/usr/local/include/$1" ];
+  then
+    sudo rm -rf /usr/local/include/$1
+  else
+    sudo rm -f /usr/local/include/$1
+  fi
+}
+
+remove_tool()
+{
+  sudo rm -f /usr/local/bin/$1
 }
 
 # $1 rootdir path
@@ -74,6 +108,7 @@ do_update()
     base="${parts[0]}"
     repo="${parts[1]}"
     dirpath=$1/$base/$(basename $repo)
+
     cmake_remove $dirpath
     repo_update $dirpath
     cmake_init $dirpath "$3" "$4"
@@ -84,10 +119,21 @@ do_update()
 }
 
 # $1 rootdir path
-# $2 repofile
 # $3 c_compiler
 # $4 cpp_compiler
 do_build()
+{
+    cmake_init "$1" "$2" "$3"
+    cmake_build "$1"
+    cmake_install "$1"
+    cmake_remove "$1"
+}
+
+# $1 rootdir path
+# $2 repofile
+# $3 c_compiler
+# $4 cpp_compiler
+do_build_all()
 {
   while read line; do
     parts=( ${line} )
@@ -95,10 +141,25 @@ do_build()
     repo="${parts[1]}"
     cmake_remove $dirpath
     dirpath=$1/$base/$(basename $repo)
-    cmake_init $dirpath "$3" "$4"
-    cmake_build $dirpath
-    cmake_install $dirpath
-    cmake_remove $dirpath
+    do_build "$dirpath" "$3" "$4"
+  done < "$2"
+}
+
+do_delete()
+{
+  while read line; do
+    parts=( ${line} )
+    kind="${parts[0]}"
+    binary="${parts[2]}"
+
+    if [[ "${kind}" == "3rdparty" ]] || [[ "${kind}" == "libraries" ]]
+    then
+      include="${parts[3]}"
+      remove_library $binary
+      remove_includes $include
+    else
+      remove_tool $binary
+    fi
   done < "$2"
 }
 
@@ -110,7 +171,14 @@ usage()
     echo "$1"
   fi
 
-  printf "Usage: %s: [-i compiler] [-u compiler] [-b compiler] -d dir [-r repofile] \n" $0
+  printf "Usage: %s: -r root-dir-path [-i compiler-family] [-u compiler-family] [-b compiler-family] [-d] [-f repo-file-path]\n" $0
+  printf "Where:\n"
+  printf "\t -r root-dir-path   - the location of the installation\n"
+  printf "\t -i compiler-family - install using gcc or clang\n"
+  printf "\t -u compiler-family - updated using gcc or clang\n"
+  printf "\t -b compiler-family - build using gcc or clang\n"
+  printf "\t -d                 - delete installed file (/usr/local/bin, /usr/local/include)\n"
+  printf "\t -f repo-file-path  - the list of repositiries to use (default ./repos.txt)\n"
   exit 2
 }
 
@@ -118,7 +186,7 @@ command=
 rootdir=
 repofile=repos.txt
 
-while getopts i:u:b:d:r: name
+while getopts di:u:b:r:f: name
 do
   case $name in
     i) command="install"
@@ -127,8 +195,9 @@ do
        compiler="$OPTARG";;
     b) command="build"
        compiler="$OPTARG";;
-    d) rootdir="$OPTARG";;
-    r) repofile="$OPTARG";;
+    d) command="delete";;
+    r) rootdir="$OPTARG";;
+    f) repofile="$OPTARG";;
     ?) usage;;
   esac
 done
@@ -159,7 +228,8 @@ then
 fi
 
 case $command in
-  install) do_install "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
-  update)  do_update  "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
-  build)   do_build   "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
+  install) do_install   "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
+  update)  do_update    "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
+  build)   do_build_all "${rootdir}" "${repofile}" "${c_compiler}" "${cpp_compiler}";;
+  delete)  do_delete    "${rootdir}" "${repofile}";;
 esac
